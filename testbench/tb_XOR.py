90fe4755e5a4224222fe1592207f97f02c1cdd08
Umesh Prasad <umeshprasadb4u@gmail.com>
2022-09-18 19:19:09 +0530

    XOR testbench

---
+++ testbench/tb_XOR.py
@@ -0,0 +1,20 @@
+from pkg.base_pkg import *
+
+@cocotb.test()
+async def tb_XOR(dut):
+      
+      D_var   = alu_library(dut)
+      
+      cocotb.start_soon(D_var.clock())
+      cocotb.start_soon(D_var.alu_enable())
+      dut.i_aluop.value = 6
+      dut.i_RD.value    = 15
+      dut.i_RA.value    = 10
+
+      for i in range(4):
+        await RisingEdge(dut.i_clk);
+        pass
+      assert dut.o_out == 5, print("AND operation mismatching")
+
+      for i in range(25):
+          await Timer(10, units = 'ns')       
