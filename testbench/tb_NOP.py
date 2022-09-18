bd196caca25eab815b358d658666ff1a0737adf7
Umesh Prasad <umeshprasadb4u@gmail.com>
2022-09-18 19:16:02 +0530

    NOP testbench

---
+++ testbench/tb_NOP.py
@@ -0,0 +1,19 @@
+from pkg.base_pkg import *
+
+@cocotb.test()
+async def tb_NOP(dut):
+      
+      D_var   = alu_library(dut)
+      
+      cocotb.start_soon(D_var.clock())
+      cocotb.start_soon(D_var.alu_enable())
+      dut.i_aluop.value = 0
+      dut.i_RD.value    = 15
+      dut.i_RA.value    = 10
+      for i in range(4):
+        await RisingEdge(dut.i_clk);
+        pass
+      assert dut.o_out == 15, print("AND operation mismatching")
+
+      for i in range(25):
+          await Timer(10, units = 'ns')       
